8e1f9207cf552ea34385bed4a4c68af31c708a97
Raz-y <raz.y054@gmail.com>
2023-07-07 12:42:43 +0300

    2. Campground Model Basics

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,12 +1,28 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((err) => {
+        console.log("error");
+        console.log(err)
+    })
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping' });
+    await camp.save();
+    res.send(camp)
 })
 app.listen(3000, () => {
     console.log('Serving on port 3000');
