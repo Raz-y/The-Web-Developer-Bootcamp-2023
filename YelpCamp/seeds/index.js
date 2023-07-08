fbcbfb4bdd4aab66f2f76c8f2dc21cface7cff5c
Raz-y <raz.y054@gmail.com>
2023-07-08 18:02:11 +0300

    Adding Basic Styles

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/9046579',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed inventore quae quaerat delectus repellat impedit soluta hic quisquam nisi, vero minima quis laudantium omnis facere ipsam corporis itaque. Officia, molestias.',
+            price: price
 
         })
         await camp.save()
