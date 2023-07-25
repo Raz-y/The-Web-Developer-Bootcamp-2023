ca242eb4ae5794261cc47054aee557a76408113d
Raz-y <raz.y054@gmail.com>
2023-07-25 23:01:48 +0300

    Controllers & Star Ratings

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -1,52 +1,21 @@
 const express = require('express')
 const router = express.Router();
 const User = require('../models/user');
+const users = require('../controllers/users')
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const { func } = require('joi');
-const { storeReturnTo } = require('../middleware')
+const { storeReturnTo } = require('../middleware');
+const user = require('../models/user');
 
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/register', (req, res) => {
-    res.render('users/register')
-})
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { username, email, password } = req.body;
-        const user = new User({ username, email });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to YelpCamp')
-            res.redirect('/campgrounds')
-        })
-    } catch (e) {
-        req.flash('error', e.message)
-        res.redirect('/register')
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-    }
+router.get('/logout', users.logout)
 
-}));
-
-router.get('/login', (req, res) => {
-    res.render('./users/login')
-})
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', `Welcome`)
-    const redirectUrl = res.locals.returnTo || '/campgrounds'
-    delete req.session.returnTo;
-    res.redirect(redirectUrl)
-})
-
-router.get('/logout', (req, res, next) => {
-    req.logOut(function (err) {
-        if (err) {
-            return next(err)
-        }
-        req.flash('success', "Goodbye");
-        res.redirect('/campgrounds')
-    })
-})
-
-module.exports = router;
+module.exports = router; 
